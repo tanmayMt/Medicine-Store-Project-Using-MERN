16b0d36f644605246b18403ad0639c5ebe8dd871
web-collab <tanmoy587@gmail.com>
2025-03-14 21:34:03 +0530

    price

--- client/src/components/Prices.js
+++ client/src/components/Prices.js
@@ -6,31 +6,26 @@ export const Prices = [
   },
   {
     _id: 1,
-    name: "₹ 99 to 100",
-    array: [99, 100],
-  },
-  {
-    _id: 2,
     name: "₹ 100 to 299",
     array: [100, 299],
   },
   {
-    _id: 3,
+    _id: 2,
     name: "₹ 300 to 599",
     array: [300, 599],
   },
   {
-    _id: 4,
+    _id: 3,
     name: "₹ 600 to 799",
     array: [600, 799],
   },
   {
-    _id: 5,
+    _id: 4,
     name: "₹ 800 to 999",
     array: [800, 999],
   },
   {
-    _id: 6,
+    _id: 5,
     name: "₹ 1000 or more",
     array: [1000, 9999],
   },
