59d6552b7dc216096bf534d372e8b93f019c5d8e
Tanmay Samanta <tanmoy587d@gmail.com>
2025-12-29 09:59:36 +0530

    d

--- bc.js
+++
@@ -1,6 +0,0 @@
-try {
-    const jwt = require('jsonwebtoken');
-    console.log('jsonwebtoken is installed.');
-} catch (error) {
-    console.error('jsonwebtoken is not installed. Please run "npm install jsonwebtoken"');
-}
